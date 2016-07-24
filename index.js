54e9265c50a0ee2a5a1997b14762c0842a9ad06a
廖之甫 <AndyLiao8@users.noreply.github.com>
2016-07-24 23:11:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 let helper            = {};
 helper.SystemHelper      = require( "./helper/systemhelper.js"    );
-module.exports.helper = helper;
 
 let pay = require("./pay/payfactory");
 pay["util"] = require('./pay/helper/util');
-module.exports.pay = pay;
+pay["helper"] = helper;
+module.exports = pay;
